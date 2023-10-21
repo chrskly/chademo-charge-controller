1478cfc85f3eb5d8b2e679ae95f7b6e6227e302c
Christian Kelly <chrskly@chrskly.com>
2023-10-21 18:23:04 +0100

    Tidy settings

--- software/settings.h
+++ software/settings.h
@@ -34,22 +34,26 @@
 #define SPI_CLK         18 // pin 24
 #define SPI_MOSI        19 // pin 25
 #define CAN_CLK_PIN     21 // pin 27
-
 #define MAIN_CAN_CS     17 // pin 22
 #define CHADEMO_CAN_CS  20 // pin 26
 
-// LED
-#define PICO_DEFAULT_LED_PIN 99
-
-#define CHARGE_INHIBIT_PIN 10 // pin 14
-
-// Chademo pins
+// Inputs
+#define PROX_PIN 11
 #define CHADEMO_IN1_PIN 14  // pin 19, CP  - contactor +ve, (sensed by 'f'), d1 enable signal, chademo plug pin 2
 #define CHADEMO_IN2_PIN 15  // pin 20, CP2 - contactor -ve, (sensed by 'g'), d2 enable signal, chademo plug pin 10
-#define CHADEMO_OUT1_PIN 12 // pin 16, CP3 - charge enable, (switched by 'k'), chademo plug pin 4
-#define CHADEMO_OUT2_PIN 13 // pin 17, contactor relay control, 'e'
-
 #define CHADEMO_CS_PIN 20   // pin 26, CS - pilot wire, input, (a.k.a 'h'), pilot signal, chademo plug pin 7
+
+// Outputs
+#define CHARGE_ENABLE_PIN 9     // pin 12
+#define CHARGE_INHIBIT_PIN 10   // pin 14
+#define CHADEMO_OUT1_PIN 12     // pin 16, CP3 - charge enable, (switched by 'k'), chademo plug pin 4
+#define CHADEMO_OUT2_PIN 13     // pin 17, contactor relay control, 'e'
+#define PICO_DEFAULT_LED_PIN 99 //
+
+
+/*
+ * General CHAdemMO settings
+ */
 
 // Version of ChaDeMo protocol to use. 1 == v0.9, 2 == v1.0
 #define CHADEMO_PROTOCOL_VERSION 1
@@ -62,32 +66,38 @@
 #define CHADEMO_RAMP_RATE 20
 #define CHADEMO_RAMP_INTERVAL 1000 // units = ms
 
-// Messages from BMS
-
-// max/min pack voltage, max dis/charge current
-#define BMS_LIMITS_MESSAGE_ID 0x351
-
-// soc
-#define BMS_SOC_MESSAGE_ID 0x355
-
-// pack voltage, current (not implemented), temp
-#define BMS_STATUS_MESSAGE_ID 0x356
-
-// Alarms/warnings from BMS
-#define BMS_ALARM_MESSAGE_ID 0x35A
-
-// If we don't see the BMS in this number of seconds, then something is wrong
-#define BMS_TTL 5
-
-// If we don't see the ChaDeMo station in this number of seconds, then something is wrong
-#define CHADEMO_STATION_TTL 2
+// If we don't receive a CAN message from the ChaDeMo station in this number of
+// seconds, then we must abort charging.
+#define CHADEMO_STATION_TTL 2000 // units = ms
 
 // This scaling factor is used to calculate max charging time from the estimated charging time
 #define MAX_CHARGING_TIME_SCALING_FACTOR 1.2
 
-//
-// Battery settings
-//
+
+/*
+ * General BMS settings
+ */
+
+// If we don't receive a CAN message from the BMS in this number of seconds,
+// then we must abort charging.
+#define BMS_TTL 5000 // units = ms
+
+// BMS CAN message which contains max/min pack voltage, max dis/charge current
+#define BMS_LIMITS_MESSAGE_ID 0x351
+
+// BMS CAN messages which contains SoC
+#define BMS_SOC_MESSAGE_ID 0x355
+
+// BMS CAN message which contains pack voltage, current (not implemented), temp
+#define BMS_STATUS_MESSAGE_ID 0x356
+
+// BMS CAN message which contains alarms/warnings from BMS
+#define BMS_ALARM_MESSAGE_ID 0x35A
+
+
+/*
+ * Battery settings
+ */
 
 #define BATTERY_MAX_VOLTAGE 350         // fixme put in proper value
 
