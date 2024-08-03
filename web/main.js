7b8feaf1768e57362efb1a1a386eeb3b5d0a101d
Christian Kelly <chrskly@chrskly.com>
2024-08-04 00:10:50 +0100

    Fix voltage graph figures

---
+++ web/main.js
@@ -0,0 +1,45 @@
+
+var statusTab = document.getElementById("status-tab");
+var batteryTab = document.getElementById("battery-tab");
+var driveTab = document.getElementById("drive-tab");
+var chargeTab = document.getElementById("charge-tab");
+var accessoriesTab = document.getElementById("accessories-tab");
+
+var ui = {
+    onLoad: function() {
+        batteryTab.classList.add("hidden");
+        driveTab.classList.add("hidden");
+        chargeTab.classList.add("hidden");
+        accessoriesTab.classList.add("hidden");
+    }
+}
+
+function showTab(tabId, buttonId) {
+    var contentContainers = document.querySelectorAll('.tab');
+    contentContainers.forEach(function(item) {
+        item.classList.add('hidden');
+    });
+    var tab = document.getElementById(tabId);
+    tab.classList.remove('hidden');
+    document.querySelectorAll('.nav-button').forEach(function(item) {
+        item.classList.remove('nav-active');
+    });
+    document.getElementById(buttonId).classList.add('nav-active');
+}
+
+function showChargeTab(tabId, buttonId) {
+    var chargeTabs = document.querySelectorAll('.charge-tab');
+    chargeTabs.forEach(function(item) {
+        item.classList.add('hidden');
+    });
+    var tab = document.getElementById(tabId);
+    tab.classList.remove('hidden');
+    document.querySelectorAll('.charge-nav-button').forEach(function(item) {
+        item.classList.remove('charge-nav-active');
+    });
+    document.getElementById(buttonId).classList.add('charge-nav-active');
+}
+
+function showVoltageTemperatureTab(){
+    
+}
